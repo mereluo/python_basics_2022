2837cf141e0ec99be5d8d53c4f34a6372899c876
mereluo <meredithluo710@gmail.com>
2022-03-27 17:14:59 -0700

    day10

---
+++ breaktheice/day10.py
@@ -0,0 +1,25 @@
+# Question 31
+# def squared_dict():
+#     result = {}
+#     for key in range(1, 21):
+#         result[key] = key ** 2
+#     return result
+
+
+# print(squared_dict())
+
+# Question 32
+# def keys():
+#     res = {i: i ** 2 for i in range(1, 21)}
+#     return list(res.keys())
+
+
+# print(keys())
+
+# Question 33
+# def squared_list():
+#     res = [i**2 for i in range(1, 21)]
+#     return res
+
+
+# print(squared_list())
